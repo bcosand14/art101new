3f420a62ae95deb661febc871ebe370fffd2c769
brooke cosand <bcosand14@gmail.com>
2026-05-19 22:02:16 -0700

    changes

---
+++ environ/env05.js
@@ -0,0 +1,38 @@
+
+$(document).ready(function() {
+
+    // Data stored in JavaScript
+    const data = {
+        sheep: "pros: open minded, creative, understanding<br>cons: naive, easily influenced, indecisive",
+        shepard: "pros: organized, efficient, relibale <br>cons: rigid, uncreative, controlling",
+        help: "Click a button to display information."
+    };
+
+    // Button 1
+    $("#btn1").click(function() {
+        $("#output").html(data.sheep);
+    });
+
+    // Button 2
+    $("#btn2").click(function() {
+        $("#output").html(data.shepard);
+    });
+
+    // Button 3
+    $("#btn3").click(function() {
+        window.location.href = "lands/sheep.html";
+    });
+
+    // Button 4
+    $("#btn4").click(function() {
+        window.location.href = "lands/shepard.html";
+    });
+
+}); 
+
+$("#image3").hover(function(){
+  alert("Hi I'm Div! I've hacked into the mainframe of your consciousness. Don't worry, I won't be in your head forever, just until you learn what you need to know to navigate this realm. I'm here to help you.");
+},
+function(){
+  alert("Bye for now, we'll talk more later.");
+});
--- exercises/garden/creature.js
+++ exercises/garden/creature.js
@@ -1,18 +1,72 @@
-// add creature to the garden
-$("#crAdd").click( function() {
+// store all creatures in an array
 
-    let crName=$("#crName").val(); 
-    console.log(crName); 
+let allCreatures=[];
 
-if( crName.length > 5) {
- $("#creature-list").append("<div>" + crName + "</div>"); 
+
+// functions
+// function to grab data from the form
+function getCreatureFromForm(){
+  
+  const freshCreature={
+        name: $("#crName").val(),
+        color: $("#crColor").val(),
+        eyesNum: $("#crEyesNum").val()
+    };
+
+return freshCreature;
+};
+
+// the checks function
+function isCreatureValid(creature) {
+  if (creature.name === "" ) return false;
+  if (creature.name.length >12) return false;
+  if ( isNaN(creature.eyesNum) || creature.eyesNum >5 ) return false;
+  return true;
 }
 
-    $("#creature-list").append("<div>" + crName + "</div>"); 
+// functions add creature to the page
+function renderCreature(creature) {
+
+    let crEyesHtml="";
+    for (let i = 0; i < creature.eyesNum; i++) {
+        crEyesHtml=crEyesHtml+ "<div class='eye'>.</div>";
+    }
+
+    const html=`
+        <div class="creature">
+            <div class="creature-body" style="background-color: ${creature.color}"> ${crEyesHtml} </div>
+            <div class="creature-info">${creature.name}</div>
+        </div>
+    `;
+
+    return html;
+}
+
+// append creature to the page
+function addCreatureToDOM(creature) {
+    const html=renderCreature(creature);
+    $("#creature-list").append(html);
+}
 
 
 
+// the main brain
+$("#crAdd").click( function(){
 
-     $("#crName").val("");
+    // create creature object from the form inputs
+    const newCreature = getCreatureFromForm();
+    console.log(newCreature);
+  
+  // safety checks
+    console.log( isCreatureValid(newCreature) );
+    if ( isCreatureValid(newCreature)==false ) { // true==false 
+     return; // stops the function which is "click" one
+    }
 
-}); 
+    // add creature to the page
+    addCreatureToDOM(newCreature)
+
+    // save to the memory 
+    // reset the fporm prepare for the next iteration
+
+});
